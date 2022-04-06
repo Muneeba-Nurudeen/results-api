f34be8ee82e3f39429d321c788a12f34807bd043
Muneeba-Nurudeen <nurudeenmuneeba99@gmail.com>
2022-04-06 15:25:39 +0000

    first commit

---
+++ results.js
@@ -0,0 +1,19 @@
+const results=[
+{name: "Wendy Dari", class: "Form One", marks:5},
+{name: "John Kennedy", class: "Form One", marks:8},
+{name: "Doris Brown", class: "Form One", marks:6},
+{name: "Kate Jones", class: "Form One", marks:4},
+{name: "Francis Elvis", class: "Form One", marks:2},
+{name: "Ashton William", class: "Form One", marks:7},
+{name: "Pascal Klein", class: "Form One", marks:5},
+{name: "Selena Charles", class: "Form One", marks:7},
+{name: "Jim Fallon", class: "Form One", marks:3},
+{name: "Bill Stone", class: "Form One", marks:4}
+
+
+]
+
+
+
+
+module.exports=results
